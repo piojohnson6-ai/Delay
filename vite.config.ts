a67cdc46b530f4e93703ab8b5469191396f474df
piojohnson6-ai <piojohnson@gmail.com>
2025-08-09 22:15:34 +0530

    l

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Delay/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
